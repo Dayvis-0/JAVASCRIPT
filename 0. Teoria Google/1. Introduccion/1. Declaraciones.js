1e4be7bf62364146c10f83d97ae0630ff0f09300
Dayvis <amdayvis39@gmail.com>
2025-04-14 21:30:27 -0500

    14/04/2025

---
+++ 0. Teoria Google/1. Introduccion/1. Declaraciones.js
@@ -0,0 +1,5 @@
+let salu = 'Hola mundo', nume = 2;
+
+if (nume === 2) {
+    console.log('Hey Holaaa')
+}
---
+++ 0. Teoria Google/1. Introduccion/2. Expresiones.js
@@ -0,0 +1,6 @@
+let nume1, nume2;
+
+nume1 =  2 + 2;
+nume2 = 2 + 2 * 4;
+
+console.log(`La suma es ${nume1}La operacionde agrupacion es: ${nume2}`);
