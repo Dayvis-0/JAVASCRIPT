69b4dd4448aad926007e82ff0f9d223020ed389e
dayvis <amdayvis39@gmail.com>
2025-08-14 23:04:09 -0500

    14/08/2025

---
+++ 5. Teoria Soy dalto mid/5. Handler, listener y objeto event..js
@@ -0,0 +1,11 @@
+const buttAddEventListe = document.querySelector("#buttAddEvent");
+
+/*El metodoaddEventListener() de la interfaz EventTarget configura una funcion que se llamara cada vez que se entregue el evento
+especificado al objetivo.
+Los objetos comunes son: Element, o sus hijos, Document, y window, pero el objeto puede ser cualquier objeto que admita 
+eventos (como IDBRequest)
+Sintaxis:
+addEventListener(type, listener)
+addEventListener(type, listener, options)
+addEventListener(type, listener, useCapture)*/
+
