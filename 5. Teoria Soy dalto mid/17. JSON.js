0b15f82be786d3b31b9e8bf64e033aa6bd4ba095
Dayvis <amdayvis39@gmail.com>
2025-09-05 22:34:44 -0500

    05/09/2025

--- 5. Teoria Soy dalto mid/17. JSON.js
+++ 5. Teoria Soy dalto mid/17. JSON.js
@@ -1,5 +1,18 @@
-const sohwJSON = document.getElementById("showJSON");
+const buttSeria = document.getElementById("buttSeria");
+const buttDeseria = document.getElementById("buttDeseria");
+
+const showSeria = document.getElementById("showSeria");
+const showDeseria = document.getElementById("showDeseria");
 
 /*Serializacion
 El proceso mediente el cual un objeto o estructura de datos se traduce a un formato adecuado para su transferencia a traves de 
 una red o almacenamiento (por ejemplo, en un búfer de matriz o formato de archivo)*/
+const serializad = '{"variable1" : "PEDRO", "variable2" : "DAYVIS"}';
+
+buttSeria.addEventListener("click", () => {
+    showSeria.textContent =  serializad;
+});
+
+buttDeseria.addEventListener("click", () => {
+    showDeseria.textContent = JSON.parse(serializad);
+});
