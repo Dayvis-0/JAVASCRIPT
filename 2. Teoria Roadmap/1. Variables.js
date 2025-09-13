f2c4252d17b1fba5fbd34fb8c6a6e68a76321c5d
Dayvis <amdayvis39@gmail.com>
2025-09-13 12:05:00 -0500

    12/09/2025 1

--- 2. Teoria Roadmap/1. Variables.js
+++ 2. Teoria Roadmap/1. Variables.js
@@ -4,6 +4,8 @@ const headingA = document.querySelector("#heading_A");
 const buttName = document.getElementById("buttName");
 const pName = document.getElementById("pName");
 const yourName = document.getElementById("yourName");
+
+const buttAna = document.getElementById("buttAna");
 const imgAna = document.getElementById("imgAna");
 
 let count = 1;
@@ -14,11 +16,22 @@ buttA.onclick = () => {
     count += 1;
 };
 
-buttName.addEventListener("click", () => {
-    pName.textContent = `Your name is: ${yourName.value}`;
-    if (imgAna.style.display === "none") {
-        imgAna.style.display = "block";
-    } else {
-        imgAna.style.display = "none";
-    }
+buttAna.addEventListener("click", () => {
+    imgAna.classList.toggle("show");
 });
+
+/*Si se escribe un programa JS de varias lineas que declara e inicializa una variable, puede declarar una variable con var
+despues de inicializarlo y seguira funcionando, esto solo funciona con var*/
+buttName.addEventListener("click", () => {
+    myName = yourName.value;
+
+    function logName() {
+        console.log(myName)
+    }
+    
+    logName();
+    
+    var myName;
+})
+
+
