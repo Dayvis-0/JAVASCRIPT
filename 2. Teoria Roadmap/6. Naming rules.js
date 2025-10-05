9b99f7cfebfeaec3cc4e485c789240471c46e42b
Dayvis <amdayvis39@gmail.com>
2025-10-05 18:47:18 -0500

    05/10/2025 1

--- 2. Teoria Roadmap/6. Naming rules.js
+++ 2. Teoria Roadmap/6. Naming rules.js
@@ -97,7 +97,80 @@ class SoftwareDeveloperOne {
 
 var meOne = new SoftwareDeveloperOne('Dayvis', 'Atao');
 
-console.log(meOne.getName());
+console.log('\n' + meOne.getName());
 
 /*Convenciones de nomencaltura de JS: PRIVATE
-*/
+Un guión bajo (_) delante de una variable, función o método en JS. Significa que es privada. Aunque
+JS no puede imponerlo, declarar algo como privado nos indica cómo debe o no usarse.
+Por ejemplo, un método privado en una clase solo debe ser utilizado internamente por la clase, pero
+se debe evitar su uso en la instancia de la clase:*/
+class SoftwareDeveloperTwo {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.name = this._getName(firstName, lastName);
+    }
+
+    _getName(firstName, lastName) {
+        return `${firstName} ${lastName}`;
+    }
+}
+
+var meTwo = new SoftwareDeveloperTwo('Dayvis', 'Atao');
+//good
+var names = meTwo.name;
+console.log('\n' + names);
+/* bad
+names1 = me._getName(me.firstName, me.lastName);
+console.log(names1); 
+Una variable/función privada también puede aparecer en un archivp JS. Esto podria significar que la
+variable/función no deberia usarse fuera de este archivo, sino solo intenamentepara calcular
+lógica de negocio adicional para otras funciones dentro del mismo archivo*/
+
+/* Convenciones de nomencaltura de JS: CONSTANT
+Por último, pero no menos importantes, hay constantes(que pretenden, ser variables inmutables) en JS
+que se escribe en letras mayúsculas.*/
+const SECONDS = 60;
+const MINUTES = 60;
+const HOURS = 24;
+const DAY = SECONDS * MINUTES * HOURS;
+const DAYS_UNTIL_TOMORROW = 1;
+
+/*Convenciones de nomencaltura de JS: GLOBAL VARIABLE
+- Una variable global de JS se declara en la parte superior de un proyecto/archivo
+- Una variable global de JS se escribe en camelCase si es mutable
+- Una variable global de JS se escirbe en MAYÚSCULAS si es inmutable*/
+
+/*Convenciones de nomencaltura de JS: DASH
+Un guion en una variable de JS tampoco es de sentido comun. Simplemente complica las cosas, es 
+como usarlos en un objeto*/
+//bad
+var person = {
+    'first-name' : 'Dayvis',
+    'last-name' : 'Atao'
+};
+var firstNameS = person['first-name'];
+//good
+var personS = {
+    firstName : 'Dayvis',
+    lastName : 'Atao'
+};
+var firstNameSS = personS.firstName;
+
+/*Convenciones de nomencaltura de JS: FILES
+Existen dos estrategias para nombrar archivos en JS. PascalCase y kebab-case. En aplicaciones frontend
+de JS, es frecuente usar PascalCase para nombrar componentes (p. eje. componentes de React)
+- components/
+--- user/
+----- UserProfile.js
+----- UserList.js
+----- UserItem.js
+--- ui/
+----- Dialog.js
+----- Dropdown.js
+----- Table.js
+Pero al contrario, en una aplicación backend de JS: kebab-case es el sentido común
+- ruting/
+--- user-route.js
+--- messages-route.js*/
+
