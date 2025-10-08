ad36b1f09c5bc4e7867290605074c630c5cd666f
Dayvis <amdayvis39@gmail.com>
2025-10-08 12:37:52 -0500

    08/10/2025

--- 2. Teoria Roadmap/12. Template literals.js
+++ 2. Teoria Roadmap/12. Template literals.js
@@ -15,7 +15,9 @@ Sintaxis:
 
 tagFunction`string text ${expression} string text`
 
-Para escapar de una comilla invertida en un literal de plantilla, coloque una barra invertida (\) antes de la comilla invertida*/
+Para escapar de una comilla invertida en un literal de plantilla, coloque una barra invertida (\) antes de la 
+comilla invertida. Escapar un carácter significa decirle al lenguaje que no lo interprete con su signiificado 
+habitual, sino que lo trate como un texto literal*/
 if (`\`` === "`") {
     console.log(true);
 }
@@ -44,3 +46,88 @@ const a = 5;
 const b = 10;
 console.log(`
 Fifteen is ${a + b} and not ${2 * a + b}`);
+
+
+/*Plantillas de anidamiento
+En ciertos casos, anidar una plantilla es la forma mas sencilla (y quizás más legible) de tener cadenas  
+configurables. Dentro de una plantilla delimitada por comillas invertidas, es sencillo permitir comillas
+invertidas internas usándolas dentro de un marcador de poosición ${expresion} dentro de una plantilla.
+Por ejemplo, sin literales de plantilla, si quisiera devolver un valor predeterminado en función
+de una condición particular, podria hacer lo siguiente*/
+function isLargeScreen() { return false; }
+const item =  {
+    isCollapsed: true
+};
+let classes1 = "header";
+classes1 += isLargeScreen()
+    ? ""
+    : item.isCollapsed
+      ? " icon-expander"
+      : " icon-expander";
+
+// Con una plantilla literal pero sin anidación podrias hacer esto:
+ const classes2 = `header ${
+    isLargeScreen() ? "" : item.isCollapsed ? "icon-expander" : "icon collapser"
+}`;
+
+// Con la anidación de literales de plantilla puedes hacer esto:
+const classes3 = `header ${
+    isLargeScreen() ? "" : `icon - ${item.isCollapsed ? "expander" : "collapser"}`
+}`;
+
+console.log('\n' + classes3);
+
+/*Plantillas etiquetadas
+Una forma mas avanzada de literales de plantilla son las plantillas etiquetadas. Las etiquetas permiten analizar 
+literales de plantilla con función. El primer argumento de una función de etiqueta contiene una matriz de 
+valores de cadena. Los demas argumentos estan relacionados con las expresiones. El nombre de la función
+utilizada para la etiqueta puede ser el que desees*/
+const person = "Mike";
+const age = 28;
+
+function myTag(strings, personExp, ageExp) {
+    const str0 = strings[0]; // "That"
+    const str1 = strings[1]; // " is a "
+    const str2 = strings[2]; // "."
+
+    const ageStr = ageExp < 100 ? "youngster" : "centenarian"; 
+
+    // Incluso podemos devolver una cadena construida usando un literal de plantilla
+    return `${str0}${personExp}${str1}${ageStr}${str2}`;
+}
+
+const outoupt = myTag`That ${person} is a ${age}.`;
+console.log('\n' + outoupt);
+
+/*La etiqueta no tiene que ser un identificador simple. Puede usar cualquier exppresión con precedencia mayor a 16,
+lo que concluye acceso a propiedades, llamadas a funciones, nuevas expresiones o incluso otro literal de plantilla*/
+console.log("\n");
+console.log`Hello`;
+console.log.bind(1,2)`Hello`;
+new Function("console.log(arguments)")`Hello`;
+
+function recursive(strings, ...values) {
+    console.log(strings, values);
+    return recursive;
+}
+
+recursive`Hello``World`;
+
+// Las funciones de etiqueta ni siquiera necesitan devolver una cadena
+function template(strings, ...keys) {
+    return (...values) => {
+        const dict = values[values.length - 1] || {};
+        const result = [strings[0]];
+        keys.forEach((key, i) => {
+            const value = Number.isInteger(key) ? values[key] : dict[key];
+            result.push(value, strings[i + 1]);
+        });
+        return result.join("");
+    };
+}
+
+const t1Closure = template`${0}${1}${2}!`;
+t1Closure("Y", "A");
+
+console.log('\n');
+console.log(t1Closure);
