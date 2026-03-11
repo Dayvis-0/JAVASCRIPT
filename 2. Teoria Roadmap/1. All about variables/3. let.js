3f1631e5b1481d191404030ae9948fbdb9232f60
Dayvis <amdayvis39@gmail.com>
2026-03-10 20:32:35 -0500

    10/03/2026 2

--- 2. Teoria Roadmap/1. All about variables/3. let.js
+++ 2. Teoria Roadmap/1. All about variables/3. let.js
@@ -1,5 +1,5 @@
-/*La declaracion let declara variables locales reasignables y con alcance de bloque, inicializando 
-opcionalmente cada una de ellas con un valor
+/*La declaracion let declara variables locales reasignables y con alcance de 
+bloque, inicializando opcionalmente cada una de ellas con un valor
 Sintaxis: 
 let name1;
 let name1 = value1;
@@ -17,8 +17,8 @@ if (x === 1) {
 
 console.log("From global "+ x);
 
-/*El alcance de una variable declarada con let es una de las siguientes sintaxis entre llaves que contiene
-mas estrechamente la declaracion let:
+/*El alcance de una variable declarada con let es una de las siguientes sintaxis 
+entre llaves que contiene más estrechamente la declaracion let:
 - Declaracion de bloque
 - Declaracion switch
 - Declaracion try...catch
@@ -31,8 +31,9 @@ O si ninguna de las anteriores aplica:
 - El ambito global, para el codigo que se ejecuta en modo script
 
 Zona muerta temporal
-Se dice que una variable declarada con let, const o class, esta en una "zona muerta temporal" desde el inicio
-del bloque hasta que la ejecucion de codigo llega al lugar donde se declara e inicializa la variable*/
+Se dice que una variable declarada con let, const o class, esta en una "zona muerta 
+temporal" desde el inicio del bloque hasta que la ejecucion de codigo llega al lugar 
+donde se declara e inicializa la variable*/
 
 {
     console.log(bar); // "Undefined"
@@ -42,9 +43,10 @@ del bloque hasta que la ejecucion de codigo llega al lugar donde se declara e in
     let foo = 2; // End to TDZ (for foo)
 }
 
-/*El termino "temporal" se utiliza porque la zona depende del orden de ejecucion (tiempo) y no del orden de 
-escritura del codigo (posicion). Por ejemplo, el siguiente codigo funciona porque, aunque la funcion que usa
-la variable "let" aparece antes de que se declare la variable, se llama fuera de la TDZ*/
+/*El termino "temporal" se utiliza porque la zona depende del orden de ejecucion 
+(tiempo) y no del orden de escritura del codigo (posicion). Por ejemplo, el siguiente 
+codigo funciona porque, aunque la funcion que usa la variable "let" aparece antes 
+de que se declare la variable, se llama fuera de la TDZ*/
 {
     const func = () => console.log(letVar); 
 
@@ -58,27 +60,31 @@ la variable "let" aparece antes de que se declare la variable, se llama fuera de
     // typeof i;
     let i = 10;
 }
-// Esto difiere del uso de typeof para variables no declaradas y variables que contienen un valor indefinido
+// Esto difiere del uso de typeof para variables no declaradas y variables que 
+// contienen un valor indefinido
 console.log(typeof undeclaredVariable);
 
-/*Nota: Las declaraciones let y const solo se procesan cuando se procesa el script actual. Si tiene dos elementos
-<script> dentro de un HTML, el primer script no esta sujeto a las restricciones de TDZ para las variables let o
-const de nivel superior declaradas en el segundo script. Sin embargo, si declara una variable let o const
+/*Nota: Las declaraciones let y const solo se procesan cuando se procesa el script 
+actual. Si tiene dos elementos <script> dentro de un HTML, el primer script no esta 
+sujeto a las restricciones de TDZ para las variables let o const de nivel superior 
+declaradas en el segundo script. Sin embargo, si declara una variable let o const
 en el primer script, volver a declararla en el segundo generara un error de redeclaracion
 
-Las declaraciones let no pueden estar en el mismo ambito que cualquier otra declaracion, incluidas las 
-declaraciones let, const, class, function, var e import*/
+Las declaraciones let no pueden estar en el mismo ambito que cualquier otra declaracion, 
+incluidas las declaraciones let, const, class, function, var e import*/
 
 {
     let fooo;
     // let fooo; SyntaxError: Identifier 'fooo' has already been declares
 }
 
-// Una declaracion let dentro del cuerpo de una funcion no puede tener el mismo nombre que un parametro
+// Una declaracion let dentro del cuerpo de una funcion no puede tener el mismo nombre 
+// que un parametro
 function foooo(aPara) {
     // let aPara = 1;
 }
-// Una declaracion let dentro de un bloque catch no puede tener el mismo nombre que el identificador asociado a catch
+// Una declaracion let dentro de un bloque catch no puede tener el mismo nombre que 
+// el identificador asociado a catch
 try {
 } catch (ePara) {
     // let ePara = 1;
@@ -111,9 +117,10 @@ switch (xParamet) {
 }
 
 /* Reglas de alcance 
-Las variables declaradas por let tienen su ámbito en el bloque para el que se declaran, así como en 
-cualquier subbloque que contenga. De esta manera, let funciona de forma muy similar a var. La principal 
-diferencia radica en que el ámbito de una variable var es la función que la contiene en su totalidad:*/
+Las variables declaradas por let tienen su ámbito en el bloque para el que se declaran, 
+así como en cualquier subbloque que contenga. De esta manera, let funciona de forma 
+muy similar a var. La principal diferencia radica en que el ámbito de una variable 
+var es la función que la contiene en su totalidad:*/
 function varTest() {
     var xVar = 1;
     {
@@ -137,7 +144,8 @@ function letTest() {
 
 letTest();
 
-// En el nivel superior de programas y funciones, let a diferencia de var, no crea una propiedad en el objeto global
+// En el nivel superior de programas y funciones, let a diferencia de var, no crea una 
+// propiedad en el objeto global
 var xVariable = "global";
 let yVariable = "global";
 
@@ -153,10 +161,11 @@ function test() {
 }
 test();
 
-/*Este fenómeno puede resultar confuso en una situación como la siguiente. La instrucción let n de n.a ya
-está dentro del ámbito del bloque del bucle for...of. Por lo tanto, el identificador n.a se resuelve
-como la propiedad a del objeto n, ubicada en la primera parte de la instrucción (let n). Esta aún se
-encuentra en la zona muerta temporal, ya que su declaración no se ha alcanzado ni terminado.*/
+/*Este fenómeno puede resultar confuso en una situación como la siguiente. La instrucción 
+let n de n.a ya está dentro del ámbito del bloque del bucle for...of. Por lo tanto, el 
+identificador n.a se resuelve como la propiedad a del objeto n, ubicada en la primera 
+parte de la instrucción (let n). Esta aún se encuentra en la zona muerta temporal, ya que
+ su declaración no se ha alcanzado ni terminado.*/
 function go(n) {
     // n aqui esta definido
     console.log("\n");
